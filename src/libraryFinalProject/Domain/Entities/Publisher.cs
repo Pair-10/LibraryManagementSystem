39db0c00360fc472a575d0bcdf7fd46a909a3bfe
kader <kader.ergin.37@gmail.com>
2024-03-23 00:59:50 +0300

    Publisher_Added

---
+++ src/libraryFinalProject/Domain/Entities/Publisher.cs
@@ -0,0 +1,30 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Domain.Entities;
+
+public class Publisher : Entity<Guid>
+{
+   
+    public string Name { get; set; } // Yayınevi adı
+    public string WebSite { get; set; } // Yayınevi web sitesi URL'i
+    public string PhoneNumber { get; set; } // Yayınevi telefon numarası
+    //ilişki kısmı
+    // Bir yayınevinn birden fazla adresii olabilir
+    public virtual ICollection<Adresse>? Addresses { get; set; } = null;//Adres tablosu ilişkisi
+    // Bir yayınevinn birden fazla materyali olabilir
+    public virtual ICollection<Material>? Materials { get; set; } = null;//materyal tablosu ilişki
+
+    public Publisher()
+    {
+    }
+    public Publisher(string name, string webSite, string phoneNumber)
+    {
+        Name = name;
+        WebSite = webSite;
+        PhoneNumber = phoneNumber;
+    }
+}
