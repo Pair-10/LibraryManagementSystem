5dc8f80961380137a4151e558371b1b6c2bb6656
kader <kader.ergin.37@gmail.com>
2024-03-23 01:01:29 +0300

    Survey_Added

---
+++ src/libraryFinalProject/Domain/Entities/Survey.cs
@@ -0,0 +1,25 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Domain.Entities;
+
+public class Survey : Entity<Guid>
+{
+
+    public string Title { get; set; } // Anket Başlığı
+    public string Description { get; set; } // Anket Açıklaması
+    //ilişki kısmı
+   //Bir anketin birden çok KullanıcıAnketi olabilir
+    public virtual ICollection<UserSurvey>? UserSurveys { get; set; } = null;
+    public Survey()
+    {
+    }
+    public Survey(string title, string description)
+    {
+        Title = title;
+        Description = description;
+    }
+}
