b8c098b15c452ea2b19e4ab56833d13784239801
kader <kader.ergin.37@gmail.com>
2024-03-23 00:57:46 +0300

    MaterialLocation_Added

---
+++ src/libraryFinalProject/Domain/Entities/MaterialLocation.cs
@@ -0,0 +1,24 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Domain.Entities;
+
+public class MaterialLocation : Entity<Guid>
+{
+    public Guid LocationId { get; set; }//
+    public Guid MaterialId { get; set; }//
+    //ilişkı kısmı
+    public virtual Location? Location { get; set; } = null;//Fk
+    public virtual Material? Material { get; set; } = null;//Fk
+    public MaterialLocation()
+    {
+    }
+    public MaterialLocation(Guid locationId, Guid materialId)
+    {
+        LocationId = locationId;
+        MaterialId = materialId;
+    }
+}
