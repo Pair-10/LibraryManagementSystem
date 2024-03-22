28c1f9a0441e572fb4a55c5395df96541973b9bc
kader <kader.ergin.37@gmail.com>
2024-03-23 00:55:16 +0300

    MaterialAdvice

---
+++ src/libraryFinalProject/Domain/Entities/MaterialAdvice.cs
@@ -0,0 +1,28 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Domain.Entities;
+
+public class MaterialAdvice : Entity<Guid>
+{  //MateryalÖnerileri
+    public Guid UserId { get; set; }//
+    public string AuthorName { get; set; }//yazarIsmi
+    public string Status { get; set; } // Durum
+
+    //ilişki kısmı
+    public virtual User? User { get; set; } = null;//ilişki Fk
+
+    public MaterialAdvice()
+    {
+    }
+
+    public MaterialAdvice(Guid userId, string authorName, string status)
+    {
+        UserId = userId;
+        AuthorName = authorName;
+        Status = status;
+    }
+}
