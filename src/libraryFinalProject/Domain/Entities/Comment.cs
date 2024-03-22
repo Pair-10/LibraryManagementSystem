95cc1baaf5830e09d41090faecc6dfc74843c6f5
kader <kader.ergin.37@gmail.com>
2024-03-23 00:16:46 +0300

    Comment_Added

---
+++ src/libraryFinalProject/Domain/Entities/Comment.cs
@@ -0,0 +1,30 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Domain.Entities;
+
+public class Comment : Entity<Guid>
+{
+    public Guid MaterialId { get; set; } //
+    public Guid UserId { get; set; } //
+    public DateTime CommentDate { get; set; } // Yorum tarihi
+    public string CommentDesc { get; set; } // Yorum açıklaması
+    //ilişki kısmı
+    public virtual Material? Material { get; set; } = null;//ilişki Fk
+    public virtual User? User { get; set; } = null;//ilişki Fk
+    public Comment()
+    {
+    }
+
+    public Comment(Guid materialId, Guid userId, DateTime commentDate, string commentDesc)
+    {
+        MaterialId = materialId;
+        UserId = userId;
+        CommentDate = commentDate;
+        CommentDesc = commentDesc;
+    }
+}
+
