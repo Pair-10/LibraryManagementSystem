04804496eaa8330a5649ead8e663873764387e44
Huseyin Karakus <vanneohuseyin@gmail.com>
2024-04-01 23:34:26 +0300

    Businessrules_updated

--- src/libraryFinalProject/Application/Features/Invoices/Commands/Update/UpdateInvoiceCommandValidator.cs
+++ src/libraryFinalProject/Application/Features/Invoices/Commands/Update/UpdateInvoiceCommandValidator.cs
@@ -7,8 +7,8 @@ public class UpdateInvoiceCommandValidator : AbstractValidator<UpdateInvoiceComm
     public UpdateInvoiceCommandValidator()
     {
         RuleFor(c => c.Id).NotEmpty();
-        RuleFor(c => c.InvoiceDate).NotEmpty();
-        RuleFor(c => c.InvoicePrice).NotEmpty();
+        RuleFor(c => c.InvoiceDate).NotEmpty().Must(BeEarlierThanNow).WithMessage("Tarih boþ ve þimdiki tarihten sonraki bir tarih olamaz.");
+        RuleFor(c => c.InvoicePrice).NotEmpty().GreaterThan(0).WithMessage("Girilen fiyat boþ veya sýfýrdan küçük olamaz.");
         RuleFor(c => c.InvoiceType).NotEmpty();
         RuleFor(c => c.Status).NotEmpty();
         RuleFor(c => c.OrderId).NotEmpty();
