481755011a92ace8202226192728babab4e3f781
Özge Kestane <ozgekestane97@gmail.com>
2024-04-30 17:26:53 +0300

    Categories&CategoryTypes&Cities&Comments&Countries&Districts messages updated - (tr&en.yaml)

--- src/libraryFinalProject/Application/Features/Comments/Commands/Update/UpdateCommentCommandValidator.cs
+++ src/libraryFinalProject/Application/Features/Comments/Commands/Update/UpdateCommentCommandValidator.cs
@@ -1,15 +1,24 @@
+using Application.Features.Comments.Constants;
 using FluentValidation;
+using NArchitecture.Core.Localization.Abstraction;
 
 namespace Application.Features.Comments.Commands.Update;
 
 public class UpdateCommentCommandValidator : AbstractValidator<UpdateCommentCommand>
 {
-    public UpdateCommentCommandValidator()
+    private ILocalizationService _localizationService;
+    public UpdateCommentCommandValidator(ILocalizationService localizationService)
     {
         RuleFor(c => c.Id).NotEmpty();
         RuleFor(c => c.MaterialId).NotEmpty();
         RuleFor(c => c.UserId).NotEmpty();
         RuleFor(c => c.CommentDate).NotEmpty();
-        RuleFor(c => c.CommentDesc).NotEmpty().MinimumLength(8).MaximumLength(200).WithMessage("Girilen yorum en az 8 en fazla 200 karakter uzunluðuunda olmalý ve boþ olmamalýdýr.");
+        RuleFor(c => c.CommentDesc).NotEmpty().MinimumLength(2).MaximumLength(200).WithMessage(GetLocalized("DescMustBeLength").Result);
+        _localizationService = localizationService;
+    }
+    public async Task<string> GetLocalized(string key)
+    {
+        return await _localizationService.GetLocalizedAsync(key, CommentsBusinessMessages.SectionName);
+
     }
 }
