1571fbbbdcb950b24f8b976f25a6824a3453afbb
Huseyin Karakus <vanneohuseyin@gmail.com>
2024-05-03 13:51:42 +0300

    notification_date_removed

--- src/libraryFinalProject/Application/Features/Notifications/Commands/Create/CreateNotificationCommandValidator.cs
+++ src/libraryFinalProject/Application/Features/Notifications/Commands/Create/CreateNotificationCommandValidator.cs
@@ -10,9 +10,6 @@ public class CreateNotificationCommandValidator : AbstractValidator<CreateNotifi
        .WithMessage("Notification description must not be empty and must be at least 7 and at most 250 characters.");
         // Bildirim açýklamasý boþ olmamalý ve en az 7, en fazla 250 karakter olmalýdýr
 
-        RuleFor(c => c.NotificationDate).NotEmpty().Must(date => date > DateTime.Today)
-        .WithMessage("The notification date must be later than today.");
-        //Bildirim tarihi bugünden ileri bir tarih olmalýdýr.
 
         RuleFor(c => c.NotificationType).NotEmpty()
         .WithMessage("Notification type should not be empty.");
